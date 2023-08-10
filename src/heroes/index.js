4ad42ab38b2958b62970037b0083068d2bfd294c
MarianoSagua <marianosagua4343@gmail.com>
2023-08-10 07:58:58 -0300

    Commit 1

---
+++ src/heroes/index.js
@@ -0,0 +1,7 @@
+export * from "./pages";
+export * from "./routes/HeroesRoutes";
+export * from "./helpers/getHeroesByPublisher";
+export * from "./components/HeroList";
+export * from "./components/HeroCard";
+export * from "./helpers/getHeroById";
+export * from "./helpers/getHeroesByName";
